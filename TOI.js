f8b9f1bf42c47813990be3a9325681dd04078025
AKshit Jariwala <AKshit Jariwala>
2017-05-02 00:11:55 +0530

    Third commit

--- TOI.js
+++ TOI.js
@@ -2,7 +2,7 @@ var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
-var port = Number(process.env.PORT || 8080);
+//var port = Number(process.env.PORT || 5000);
 
 var Datasource = require('nedb');
 
@@ -51,7 +51,7 @@ app.get('/scrape',function(req,res){
 			
 })
 
-app.listen(process.env.PORT || 8081,function(){
+app.listen(process.env.PORT || 5000,function(){
 	console.log("RUNNING AT 8081..");
 })
 
