97d56b475f60f25a8f7fd2014ad3b96f17316b35
AKshit Jariwala <AKshit Jariwala>
2017-05-01 19:34:13 +0530

    first commit

---
+++ FetchDB.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+	app = express(),
+	port = Number(process.env.PORT || 8080);
+	
+	//var mongo_data = require('');	
+	var Datasource = require('nedb');
+	
+	var db = new Datasource({
+		filename: 'headlines.db', // provide a path to the database file 
+		autoload: true, // automatically load the database
+		timestampData: true // automatically add and manage the fields createdAt and updatedAt
+	});
+
+	
+	app.get('/', function(req, res) {
+		
+		var result = [];
+		db.find({}).sort({
+		updatedAt: -1
+	}).exec(function(err,headlines) {
+		if (err) res.send(err);
+		//res.json(news);
+		headlines.foreach(function(d)
+		{
+			result.push({"Headline":d.Headline});
+		});
+		res.json(result);
+		});
+	});
+
+	app.listen(port, function() {
+		console.log('Listening on port ' + port);
+	});
---
+++ SoaProject/TOI.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var app = express();
+var port = Number(process.env.PORT || 8080);
+
+var Datasource = require('nedb');
+
+//var headlines =  new m({filename:'data.db'});
+var db = new Datasource(
+{
+	filename:'headlines.db',
+	autoload:true,
+	timestampData:false
+}
+);
+	
+app.get('/scrape',function(req,res){
+
+
+		url = 'http://timesofindia.indiatimes.com/';
+		
+		request(url,function(error,response,html)
+		{
+			if(!error)
+			{
+				var $ = cheerio.load(html);
+				var headline = [];
+				
+				console.log("-----------------------------------------------Today's Headlines------------------------------------------------------");
+				//headline.push({"Headline":$(".top-story > .list8 > li > a[href]").text()});
+				//headline = $(".top-story > .list8 > li > a[href]").text();
+				//console.log("\r\nHeadline:" +headline);
+				
+				for(var i=0;i<1;i++)
+				{
+					headline.push({"Headline":$(".top-story > .list8 > li ").text()});
+					console.log("Headline : " +headline[i].Headline);
+				}
+			
+			db.insert(headline,function(err,newGoal)
+			{
+				if(err)
+					console.log(err);
+				console.log(newGoal);
+			});
+			
+			res.send("Entries are successfully added");
+			}
+		})
+			
+})
+
+app.listen('8081',function(){
+	console.log("RUNNING AT 8081..");
+})
+
+exports = module.experts = app;
