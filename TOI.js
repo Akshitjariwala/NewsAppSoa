e278c13505d5085e913a01c7faf394afe6017e86
AKshit Jariwala <AKshit Jariwala>
2017-05-03 01:48:50 +0530

    Final Commit

--- TOI.js
+++ TOI.js
@@ -27,29 +27,41 @@ app.get('/scrape',function(req,res){
 				var $ = cheerio.load(html);
 				var headline = [];
 				
-				console.log("-----------------------------------------------Today's Headlines------------------------------------------------------");
+				console.log("-------------------------------------------------------------------------Today's Headlines--------------------------------------------------------------------------------");
 				//headline.push({"Headline":$(".top-story > .list8 > li > a[href]").text()});
 				//headline = $(".top-story > .list8 > li > a[href]").text();
 				//console.log("\r\nHeadline:" +headline);
+				//res.writeHead(200, { 'Content-Type': 'text/html' });
+				/*for(var i=0;i<1;i++)
+				{	
+					//res.writeHead(200, { 'Content-Type': 'text/html' });
+					//console.log("\r\n");
+					//headline.push({"Headline":$(".top-story > .list8 > li ").text()});
+					//res.send('<br>');
+					//res.send("Headline : "  + "'<br>'" +headline[i].Headline + "'<br>'");
+					//headline[i]
+					
+				}*/
 				
-				for(var i=0;i<1;i++)
-				{
-					headline.push({"Headline":$(".top-story > .list8 > li ").text()});
-					console.log("Headline : " +headline[i].Headline);
-				}
+					$('.top-story > .list8 ').each(function(i,e)
+					{
+						res.send("Headlines : <br>  " + $(this).text());
+					});
+					
 			
-			db.insert(headline,function(err,newGoal)
+			/*db.insert(headline,function(err,newGoal)
 			{
 				if(err)
 					console.log(err);
 				console.log(newGoal);
-			});
+			});*/
 			
-			res.send("Entries are successfully added");
+			//res.send("Entries are successfully added");
 			}
 		})
 			
 })
+
 
 app.listen(process.env.PORT || 5000,function(){
 	console.log("RUNNING AT 8081..");
